2708fc49ada8d069146eb66541260985e49b185f
matheus.cordeiro <matheus.cordeiro@NTB-SDE-35>
2019-07-05 14:57:54 -0300

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEnumerable<Noticia> todasAsNoticias;
+
+        public HomeController()
+        {
+            todasAsNoticias = new Noticia().TodasAsNoticias().OrderBy(x => x.Data);
+            
+        }
+        public ActionResult Index()
+        {
+            var ultimasNoticias = todasAsNoticias.Take(3);
+            var todasAscategorias = todasAsNoticias.Select(x => x.Categoria).Distinct().ToList();
+
+            ViewBag.Categorias = todasAscategorias;
+            return View(ultimasNoticias);
+        }
+
+        public ActionResult TodasAsNoticias()
+        {
+
+            return View(todasAsNoticias);
+        }
+
+        public ActionResult MostrarNoticia(int noticiaId, string titulo, string categoria)
+        {
+
+            return View(todasAsNoticias.FirstOrDefault(x=> x.NoticiaId == noticiaId));
+
+        }
+
+        public ActionResult MostraCategoria(string categoria)
+        {
+            var categoriaEspecifica = todasAsNoticias.Where(x => x.Categoria.ToLower() == categoria.ToLower()).ToList();
+            ViewBag.Categoria = categoria;
+            return View(categoriaEspecifica);
+        }
+
+
+    }
+}
---
+++ WebApplication2/Models/Noticia.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Noticia
+    {
+
+        public int NoticiaId { get; set; }
+        public string Categoria { get; set; }
+        public string Titulo { get; set; }
+      
+
+        public string Conteudo { get; set; }
+        public DateTime Data { get; set; }
+
+
+
+
+
+        public IEnumerable<Noticia> TodasAsNoticias()
+        {
+            var retorno = new Collection<Noticia>
+            {
+
+
+
+                new Noticia
+                {
+                    NoticiaId = 1,
+                    Categoria = "Policial",
+                    Titulo = "teste titulo policial",
+                    Conteudo="o Bandido....",
+                    Data= new DateTime(2019,3,1)
+
+
+                },
+
+                   new Noticia
+                {
+                    NoticiaId = 2,
+                    Categoria = "Humor",
+                    Titulo = "teste titulo comedia ",
+                    Conteudo="os comediantes mais engraçados....",
+                    Data= new DateTime(2019,3,31)
+
+
+                },
+
+                         new Noticia
+                {
+                    NoticiaId = 3,
+                    Categoria = "Vendas",
+                    Titulo = "venda",
+                    Conteudo="vendo esta casa ...",
+                    Data= new DateTime(2019,3,31)
+
+
+                },
+
+        };
+            return retorno;
+         }
+
+    }
+}
